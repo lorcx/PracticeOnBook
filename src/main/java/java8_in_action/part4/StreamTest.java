e15178b8b929719df7cc8a7f072933b3c6c1c117
lorcx <740707954@qq.com>
2018-03-20 23:26:41 +0800

    sj

---
+++ src/main/java/java8_in_action/part4/StreamTest.java
@@ -0,0 +1,35 @@
+package java8_in_action.part4;
+
+import java8_in_action.entiry.Dish;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @Author: lx
+ * @Date: Created in 2018/3/20 0020
+ */
+public class StreamTest {
+    public static void main(String[] args) {
+        /*
+        * Dish.menu 相当于元素序列
+        * filter、map、limit相当于流水线，执行这些操作只是加入到流水线中并没有执行
+        * collect最终执行
+        * */
+        List<String> threeHighCaloricDishName = Dish.menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(threeHighCaloricDishName);
+
+
+        // 流只能被遍历一次
+        List<String> title = Arrays.asList("java", "IKN", "qwe");
+        Stream<String> s = title.stream();
+        s.forEach(System.out::print);
+        s.forEach(System.out::print);// tream has already been operated upon or closed
+    }
+}
